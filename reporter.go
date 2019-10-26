91f485315be6848eb469a81c50cd3bbbafcc949f
Vladimir Lashko <vladimir@clutch.co>
2019-10-26 00:10:49 -0400

    Fixes existed output

--- reporter.go
+++ reporter.go
@@ -85,7 +85,7 @@ func (ow *outputWriter) WriteCallSuccess(label string, c *C) {
 func renderCallHeader(label string, c *C, prefix, suffix string) string {
 	pc := c.method.PC()
 
-	out := fmt.Sprintf("%s%s %s: %s%s", prefix, label, niceFuncPath(pc),
+	out := fmt.Sprintf("%s%s: %s: %s%s", prefix, label, niceFuncPath(pc),
 		niceFuncName(pc), suffix)
 
 	switch strings.ToLower(*formattedMessageFlag) {
